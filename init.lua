e05cbb2fff2fa15a87386952bb0e6226212b0071
Heather <gitmail@emptystar.email>
2024-07-22 22:30:01 -0400

    Do not activate if other mods already show item names

--- init.lua
+++ init.lua
@@ -1,5 +1,10 @@
 -- Based on 4itemnames mod by 4aiman
 
+-- Function to determine if show item name functionality is already implemented
+local function show_already_implemented()
+	return minetest.get_modpath("unified_inventory") and true or false
+end
+
 local wield = {}
 local wieldindex = {}
 local huds = {}
@@ -69,67 +74,68 @@ local function get_first_line(text)
 	return text
 end
 
-minetest.register_globalstep(function(dtime)
-	for _, player in pairs(minetest.get_connected_players()) do
-		local player_name = player:get_player_name()
-		local wstack = player:get_wielded_item()
-		local wname = wstack:get_name()
-		local windex = player:get_wield_index()
+if not show_already_implemented() then
+	minetest.register_globalstep(function(dtime)
+		for _, player in pairs(minetest.get_connected_players()) do
+			local player_name = player:get_player_name()
+			local wstack = player:get_wielded_item()
+			local wname = wstack:get_name()
+			local windex = player:get_wield_index()
 
-		if dtimes[player_name] and dtimes[player_name] < dlimit then
-			dtimes[player_name] = dtimes[player_name] + dtime
-			if dtimes[player_name] > dlimit and huds[player_name] then
-				player:hud_change(huds[player_name], 'text', "")
+			if dtimes[player_name] and dtimes[player_name] < dlimit then
+				dtimes[player_name] = dtimes[player_name] + dtime
+				if dtimes[player_name] > dlimit and huds[player_name] then
+					player:hud_change(huds[player_name], 'text', "")
+				end
 			end
-		end
 
-		-- Update HUD when wielded item or wielded index changed
-		if wname ~= wield[player_name] or windex ~= wieldindex[player_name] then
-			wieldindex[player_name] = windex
-			wield[player_name] = wname
-			dtimes[player_name] = 0
+			-- Update HUD when wielded item or wielded index changed
+			if wname ~= wield[player_name] or windex ~= wieldindex[player_name] then
+				wieldindex[player_name] = windex
+				wield[player_name] = wname
+				dtimes[player_name] = 0
 
-			if huds[player_name] then 
+				if huds[player_name] then 
 
-				-- Get description (various fallback checks for old Minetest versions)
-				local def = minetest.registered_items[wname]
-				local desc
-				if wstack.get_short_description then
-					-- get_short_description()
-					desc = wstack:get_short_description()
-				end
-				if (not desc or desc == "") and wstack.get_description then
-					-- get_description()
-					desc = wstack:get_description()
-					desc = get_first_line(desc)
-				end
-				if (not desc or desc == "") and not wstack.get_description then
-					-- Metadata (old versions only)
-					local meta = wstack:get_meta()
-					desc = meta:get_string("description")
-					desc = get_first_line(desc)
-				end
-				if not desc or desc == "" then
-					-- Item definition
-					desc = def.description
-					desc = get_first_line(desc)
-				end
-				if not desc or desc == "" then
-					-- Final fallback: itemstring
-					desc = wname
-				end
-
-				-- Print description
-				if desc then
-					-- Optionally append the 'technical' itemname
-					local tech = minetest.settings:get_bool("show_wielded_item_itemname", false)
-					if tech and desc ~= "" then
-						desc = desc .. " ["..wname.."]"
+					-- Get description (various fallback checks for old Minetest versions)
+					local def = minetest.registered_items[wname]
+					local desc
+					if wstack.get_short_description then
+						-- get_short_description()
+						desc = wstack:get_short_description()
 					end
-					player:hud_change(huds[player_name], 'text', desc)
+					if (not desc or desc == "") and wstack.get_description then
+						-- get_description()
+						desc = wstack:get_description()
+						desc = get_first_line(desc)
+					end
+					if (not desc or desc == "") and not wstack.get_description then
+						-- Metadata (old versions only)
+						local meta = wstack:get_meta()
+						desc = meta:get_string("description")
+						desc = get_first_line(desc)
+					end
+					if not desc or desc == "" then
+						-- Item definition
+						desc = def.description
+						desc = get_first_line(desc)
+					end
+					if not desc or desc == "" then
+						-- Final fallback: itemstring
+						desc = wname
+					end
+
+					-- Print description
+					if desc then
+						-- Optionally append the 'technical' itemname
+						local tech = minetest.settings:get_bool("show_wielded_item_itemname", false)
+						if tech and desc ~= "" then
+							desc = desc .. " ["..wname.."]"
+						end
+						player:hud_change(huds[player_name], 'text', desc)
+					end
 				end
 			end
 		end
-	end
-end)
-
+	end)
+end
